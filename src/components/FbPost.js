a7ba73499789e3f3a617c0d59a6497fb8efa44c0
premod1 <premodsuraweera1@gmail.com>
2023-01-20 23:29:32 +0530

    done

--- src/components/FbPost.js
+++ src/components/FbPost.js
@@ -3,40 +3,308 @@ function FbPost() {
   return (
     <div className="post-container">
       <div className="postWrapper">
+        <div>
+          <h1 style={{ color: "white", fontWeight: "bold", fontSize: "50px" }}>
+            News and Blogs
+          </h1>
+        </div>
+
         <iframe
           src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02d8iecTwEehu97PQnuSQWUgR6ap8vV48Bja52EtB58NQUaDa7mgzXYakeb97Rwkdcl&show_text=true&width=500"
-          // width="500"
-          // height="654"
-          // style="border:none;overflow:hidden"
-          // scrolling="no"
-          // frameborder="0"
-          // allowfullscreen="true"
-          // allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "12%",
+            left: "3%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "120%",
+          }}
         ></iframe>
-        <div>
-          <iframe
-            src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02xQvMiM2YPWsyajMKPjQeNWE3fLZj3nvHLvfxnWeaVy56DvtyKRTRK2dxpnDxCiE1l&show_text=true&width=500"
-            // width="500"
-            // height="660"
-            // style="border:none;overflow:hidden"
-            // scrolling="no"
-            // frameborder="0"
-            // allowfullscreen="true"
-            // allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          ></iframe>
-          <div>
-            <iframe
-              src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0tnN78poDvWzz4j2jyTkE1kiPztZna84NnqGTWPJhqdBk8haBuBLhVP4f1KP9gLLdl&show_text=true&width=500"
-              //   width="500"
-              //   height="718"
-              //   style="border:none;overflow:hidden"
-              //   scrolling="no"
-              //   frameborder="0"
-              //   allowfullscreen="true"
-              //   allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-            ></iframe>
-          </div>
-        </div>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02xQvMiM2YPWsyajMKPjQeNWE3fLZj3nvHLvfxnWeaVy56DvtyKRTRK2dxpnDxCiE1l&show_text=true&width=500"
+          title="news"
+          width="200px"
+          height="450px"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "12%",
+            left: "35%",
+            right: "0",
+            bottom: "10%",
+            width: "25%",
+            height: "120%",
+          }}
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0tnN78poDvWzz4j2jyTkE1kiPztZna84NnqGTWPJhqdBk8haBuBLhVP4f1KP9gLLdl&show_text=true&width=500"
+          title="news"
+          width="200px"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "12%",
+            left: "70%",
+            right: "0",
+            bottom: "50%",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02N6ZqXxd4hAXnWEyfabr4emp1tLWeC96eq2GqxEMx3CGicG8UhbjnZUfvYSWgVY6hl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "150%",
+            left: "3%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "120%",
+          }}
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid022dmfRx3RDzdbuC8LsR19ncJu68BJ6DT5gFd5eTA8dBzLMNgwGFuTR7vNouTM4ngJl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "150%",
+            left: "35%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "120%",
+          }}
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid04v8nJ3KXgUB4ZmPS4Pk8d37EcAZhoPUGF3yu8tQ9k3ui5DLuByaeMBeMde32xMPfl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "150%",
+            left: "70%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0aRZpE3miAGNZ2WWQahLc9JqRbBVYZ4PeqKSf9oqoDUoKfPbUc2g5DijqjNMBZTdQl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "300%",
+            left: "3%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0ymzNLJMXxaZCKXwD75GAqkL7SAPPKGxNf4L4rvazBJM3RLAYEC1UanCgdYerfmRxl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "300%",
+            left: "35%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0pRsVXVVPNaCtDEvb1VZCjfqyHqACMrB4aLEgpUk3uimxPoWBmd98tfr6oBa9fW25l&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "300%",
+            left: "70%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fvideos%2F3382528035338195%2F&show_text=true&width=476&t=0"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "450%",
+            left: "3%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02e9h7KuVXyGdiptXuyF2L511JJg3QtjP2r4UkZwrtkX3tLJwgPVtqh6vkKP8z7rRXl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "450%",
+            left: "35%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02RCyuvmSkMimVd53Wef78LkFJ2cjVixgPjkhH4b6M63rzSzwtsDGGLVi5k8p8cf5Dl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "450%",
+            left: "70%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0NpcZPD6TYfk23t8cQiYStw5M3cs5P197YdL9mnXKcj9MuakQ9Rsh2Cp5SJwt1a5Pl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "600%",
+            left: "3%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02apizwCmRsKyXpdNeb1B3PtaaoqhK45knBx1dP2PzwmUZjVQyKVKeaiQE9kjN6nZl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "600%",
+            left: "35%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fvideos%2F774466027109231%2F&show_text=true&width=476&t=0"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "600%",
+            left: "70%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0Gw1DyGHRcGZPqq6BXVQfdi8Kr9nM5r1GdJFUhU4zJykmdfw7gsJ7ghoah9GqDqsLl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "750%",
+            left: "3%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid02d4TcgZW2SsBa6qWGFsN2JuXh8BMu8eBYPpEHzjChhHoyfN3668Vm7En5xKun4qhnl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "750%",
+            left: "35%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FSentabInternational%2Fposts%2Fpfbid0f5Xu5oqVFb8hxDJEVge5bTFiWtirtbCFQrJxJoXUn2QXexKmtGy2dWLVL6NkkhgMl&show_text=true&width=500"
+          title="news"
+          width="200"
+          height="200"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: "750%",
+            left: "70%",
+            right: "0",
+            bottom: "0",
+            width: "25%",
+            height: "130%",
+          }}
+        ></iframe>
       </div>
     </div>
   );
